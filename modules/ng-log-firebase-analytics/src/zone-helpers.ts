14a1178a9b1111aa20965c33c329e86f4642b101
Moe Myat Zaw <mmzliveid@gmail.com>
2020-06-17 20:09:33 +0630

    Rename to zone-helpers.ts

---
+++ modules/ng-log-firebase-analytics/src/zone-helpers.ts
@@ -0,0 +1,27 @@
+import { NgZone } from '@angular/core';
+
+import { Observable } from 'rxjs';
+
+export const runOutsideAngular = (zone: NgZone) => <T>(obs$: Observable<T>): Observable<T> => {
+    return new Observable<T>((subscriber) => {
+        return zone.runOutsideAngular(() => {
+            return obs$.subscribe(
+                (value) => {
+                    zone.run(() => {
+                        subscriber.next(value);
+                    });
+                },
+                (error) => {
+                    zone.run(() => {
+                        subscriber.error(error);
+                    });
+                },
+                () => {
+                    zone.run(() => {
+                        subscriber.complete();
+                    });
+                }
+            );
+        });
+    });
+};
