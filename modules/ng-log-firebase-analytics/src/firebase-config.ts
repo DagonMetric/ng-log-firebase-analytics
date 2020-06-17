a03ccc1585db601eefb40cd02f7caf9027e84c7e
Moe Myat Zaw <mmzliveid@gmail.com>
2020-06-17 20:12:08 +0630

    Add firebase-config.ts

---
+++ modules/ng-log-firebase-analytics/src/firebase-config.ts
@@ -0,0 +1,18 @@
+/**
+ * @license
+ * Copyright DagonMetric. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found under the LICENSE file in the root directory of this source tree.
+ */
+
+export interface FirebaseConfig {
+    apiKey?: string;
+    projectId?: string;
+    appId?: string;
+    authDomain?: string;
+    databaseURL?: string;
+    storageBucket?: string;
+    messagingSenderId?: string;
+    [key: string]: string | undefined;
+}
