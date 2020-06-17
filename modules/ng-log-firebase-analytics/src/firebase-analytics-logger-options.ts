6966de8d70ec3349833cb4315f72507c609253f5
Moe Myat Zaw <mmzliveid@gmail.com>
2020-06-17 20:23:08 +0630

    Update firebase-analytics-logger-options.ts

--- modules/ng-log-firebase-analytics/src/firebase-analytics-logger-options.ts
+++ modules/ng-log-firebase-analytics/src/firebase-analytics-logger-options.ts
@@ -8,20 +8,22 @@
 
 import { InjectionToken } from '@angular/core';
 
-export interface FirebaseOptions {
-    measurementId: string;
+export interface FirebaseConfig {
     apiKey?: string;
+    projectId?: string;
+    appId?: string;
     authDomain?: string;
     databaseURL?: string;
-    projectId?: string;
     storageBucket?: string;
     messagingSenderId?: string;
-    appId?: string;
+    [key: string]: string | undefined;
 }
 
 export interface FirebaseAnalyticsLoggerOptions {
-    firebase: FirebaseOptions;
+    firebase: FirebaseConfig;
     appName?: string;
 }
 
-export const FIREBASE_ANALYTICS_LOGGER_OPTIONS_TOKEN = new InjectionToken<FirebaseAnalyticsLoggerOptions>('FirebaseAnalyticsLoggerOptions');
+export const FIREBASE_ANALYTICS_LOGGER_OPTIONS_TOKEN = new InjectionToken<FirebaseAnalyticsLoggerOptions>(
+    'FirebaseAnalyticsLoggerOptions'
+);
