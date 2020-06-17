ae9a8176d801ae66ec1c8b67b47f8fa39fcc29dd
Moe Myat Zaw <mmzliveid@gmail.com>
2020-06-17 20:10:11 +0630

    Update zone-helpers.ts

--- modules/ng-log-firebase-analytics/src/zone-helpers.ts
+++ modules/ng-log-firebase-analytics/src/zone-helpers.ts
@@ -1,27 +1,35 @@
-import { NgZone } from '@angular/core';
+/**
+ * @license
+ * Copyright DagonMetric. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found under the LICENSE file in the root directory of this source tree.
+ */
 
-import { Observable } from 'rxjs';
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 
-export const runOutsideAngular = (zone: NgZone) => <T>(obs$: Observable<T>): Observable<T> => {
-    return new Observable<T>((subscriber) => {
-        return zone.runOutsideAngular(() => {
-            return obs$.subscribe(
-                (value) => {
-                    zone.run(() => {
-                        subscriber.next(value);
-                    });
-                },
-                (error) => {
-                    zone.run(() => {
-                        subscriber.error(error);
-                    });
-                },
-                () => {
-                    zone.run(() => {
-                        subscriber.complete();
-                    });
-                }
-            );
-        });
-    });
-};
+import { SchedulerAction, SchedulerLike, Subscription, queueScheduler } from 'rxjs';
+
+export class ZoneScheduler implements SchedulerLike {
+    constructor(private zone: any, private delegate = queueScheduler) {}
+
+    now(): number {
+        return this.delegate.now();
+    }
+
+    schedule(work: (this: SchedulerAction<any>, state?: any) => void, delay?: number, state?: any): Subscription {
+        const targetZone = this.zone;
+
+        const workInZone = function (this: SchedulerAction<unknown>, stateLocal: unknown) {
+            targetZone.runGuarded(() => {
+                work.apply(this, [stateLocal]);
+            });
+        };
+
+        return this.delegate.schedule(workInZone, delay, state);
+    }
+}
